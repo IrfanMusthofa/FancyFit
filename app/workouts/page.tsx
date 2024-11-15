6e0f8fd84455fc4121923845a10f450c0c6e3a42
Irfan Musthofa <im@irfanmusthofa.com>
2024-11-15 23:01:53 +0700

    again

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -41,40 +41,6 @@ const workoutPages = [
   "body-workout/traps-mid-back",
   "body-workout/triceps",
 ];
-// //@ts-ignore
-// export async function updateUserData(currentUser, workoutData: string) {
-//   console.log("currentUser", currentUser);
-//   // Fetch the current user data from the database
-//   const user = await db.user.findUnique({
-//     where: { clerkUserId: currentUser },
-//   });
-
-//   if (user) {
-//     const existingWorkouts = user.workout || [];
-
-//     if (existingWorkouts.includes(workoutData)) {
-//       // Remove the workout if it already exists
-//       await db.user.update({
-//         where: { clerkUserId: currentUser },
-//         data: { workout: existingWorkouts.filter((w) => w !== workoutData) },
-//       });
-//     } else {
-//       // Add the workout if it does not exist
-//       await db.user.update({
-//         where: { clerkUserId: currentUser },
-//         data: { workout: [...existingWorkouts, workoutData] },
-//       });
-//     }
-//   }
-// }
-
-// //@ts-ignore
-// export async function getAllWorkoutData(currentUser) {
-//   return await db.user.findUnique({
-//     where: { clerkUserId: currentUser },
-//     select: { workout: true },
-//   });
-// }
 
 const workouts = async () => {
   const user = await currentUser();
